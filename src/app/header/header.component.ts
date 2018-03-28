4d727badda206e5fa170afdfed849978fe3cdf5e
Wonder Woman <kritii.bhattarai@gmail.com>
2018-03-29 03:08:30 +0545

    Followed mentioned workflow, header, footer components, sticky header and mobile menu toggle feature!

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // Public property of boolean type set false by default
+  public navIsFixed: boolean = false;
+  public menuIsOpened: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: any) { }
 
   ngOnInit() {
   }
 
+  // == Setup click event to toggle mobile menu
+  toggleState() { // click handler
+    let bool = this.menuIsOpened;
+    this.menuIsOpened = bool === false ? true : false;
+  }
+
+  // == Listeing scroll event for window object
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    let number = window.scrollY || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
+    if (number > document.getElementById('mastHead').offsetHeight) {
+      this.navIsFixed = true;
+    } else if (this.navIsFixed && number < 10) {
+      this.navIsFixed = false;
+    }
+  }
 }
