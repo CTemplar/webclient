8fc77fa228d51cdbf96716ca2c9479b9a3f9789a
user <user@users-iMac-58.local>
2018-05-11 09:57:09 -0500

    build optimizer issue

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LoadingComponent implements OnInit {
   @Input('isLoading') isLoading?: boolean;
-  @Input('quote') quote?: object;
+  @Input('quote') quote?: any;
   constructor() {}
 
   ngOnInit() {}
