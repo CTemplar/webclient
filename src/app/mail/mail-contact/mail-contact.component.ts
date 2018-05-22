ea993b3ec0bc880c2bf24055aa4a8952ab1e0fd3
Wonder Woman <kritii.bhattarai@gmail.com>
2018-05-23 01:12:52 +0545

    Completed mail contact page

--- src/app/mail/mail-contact/mail-contact.component.ts
+++ src/app/mail/mail-contact/mail-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons, NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-mail-contact',
@@ -9,17 +10,32 @@ export class MailContactComponent implements OnInit {
 
 	isLayoutSplitted: boolean = false;
 
-	constructor() { }
+	constructor(private modalService: NgbModal, config: NgbDropdownConfig) {
+		// customize default values of dropdowns used by this component tree
+		config.autoClose = "outside";
+	}
 
 	ngOnInit() {
 	}
 
 	initSplitContactLayout():any {
     	this.isLayoutSplitted = true;
+
+    	if (this.isLayoutSplitted === true) {
+    		window.document.documentElement.classList.add('no-scroll');
+    	}
 	}
 
 	destroySplitContactLayout():any {
     	this.isLayoutSplitted = false;
+
+    	if (this.isLayoutSplitted === false) {
+    		window.document.documentElement.classList.remove('no-scroll');
+    	}
 	}
 
+	// == Open change password NgbModal
+	addUserContactModalOpen(addUserContent) {
+		this.modalService.open(addUserContent, {centered: true, windowClass: 'modal-sm users-action-modal'});
+	}
 }
