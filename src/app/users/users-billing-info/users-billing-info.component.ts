68e63c893a60afc51d7ba53e69342ac6b6c4b3c5
Ahsin Siddique <ahsin.siddique@gmail.com>
2018-07-14 23:35:42 +0500

    update billing info

--- src/app/users/users-billing-info/users-billing-info.component.ts
+++ src/app/users/users-billing-info/users-billing-info.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 // Service
 import { SharedService } from '../../store/services';
-import { FinalLoading } from '../../store/actions';
+import { CreateNewWallet, FinalLoading, GetBitcoinValue } from '../../store/actions';
 import { Store } from '@ngrx/store';
-import { AuthState } from '../../store/datatypes';
+import { AppState, BitcoinState } from '../../store/datatypes';
 
-import { FormBuilder, FormGroup, Validators, PatternValidator, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users-billing-info',
@@ -16,8 +16,9 @@ import { FormBuilder, FormGroup, Validators, PatternValidator, AbstractControl }
 export class UsersBillingInfoComponent implements OnDestroy, OnInit {
 
   constructor(private sharedService: SharedService,
-              private store: Store<AuthState>,
-              private formBuilder: FormBuilder) { }
+              private store: Store<AppState>,
+              private formBuilder: FormBuilder) {
+  }
 
   cardNumber;
   billingForm: FormGroup;
@@ -26,6 +27,9 @@ export class UsersBillingInfoComponent implements OnDestroy, OnInit {
   cvc;
   months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
   years = ['2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026'];
+  paymentMethod: string = null;
+  count: number = 30;
+  bitcoinState: BitcoinState;
 
   ngOnInit() {
     this.sharedService.hideFooter.emit(true);
@@ -34,6 +38,16 @@ export class UsersBillingInfoComponent implements OnDestroy, OnInit {
     this.billingForm = this.formBuilder.group({
       'cardNumber': ['', [Validators.minLength(16), Validators.maxLength(16)]]
     });
+  }
+
+  timer() {
+    if (this.count < 1) {
+      return;
+    }
+    this.count--;
+    setTimeout(() => {
+      this.timer();
+    }, 1000);
   }
 
   getToken() {
@@ -49,6 +63,23 @@ export class UsersBillingInfoComponent implements OnDestroy, OnInit {
       } else {
         // console.log(response.error.message);
       }
+    });
+  }
+
+  selectBitcoinMethod() {
+    this.store.dispatch(new GetBitcoinValue());
+    this.timer();
+    this.paymentMethod = 'bitcoin';
+    this.store.select(state => state.bitcoin).subscribe((bitcoinState: BitcoinState) => {
+      this.bitcoinState = bitcoinState;
+    });
+    this.createNewWallet();
+  }
+
+  createNewWallet() {
+    this.store.dispatch(new CreateNewWallet());
+    this.store.select(state => state.bitcoin).subscribe((bitcoinState: BitcoinState) => {
+      this.bitcoinState.newWalletAddress = bitcoinState.newWalletAddress;
     });
   }
 
