fc12edf4f662dce0d6b577d183e1f58565b9f656
Zain Zafar <zain@aurorasolutions.io>
2018-08-03 21:51:08 +0500

    remove hideFreePlan property and reuse isPrime

--- src/app/shared/components/pricing-plans/pricing-plans.component.ts
+++ src/app/shared/components/pricing-plans/pricing-plans.component.ts
@@ -19,7 +19,6 @@ export class PricingPlansComponent implements OnInit, OnDestroy {
   @Input('blockGapsZero') blockGapsZero: boolean; // Flag to add top and bottom gap conditionally
   @Input() showCurrentPlan: boolean;
   @Input() isPrime: boolean;
-  @Input() hideFreePlan: boolean;
 
   constructor(
     private sharedService: SharedService,
