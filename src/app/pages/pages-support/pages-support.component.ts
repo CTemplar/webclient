8b2c5b385c2d2b0dd6f2590bd0b8f943496e67ad
Zain Zafar <zain@aurorasolutions.io>
2018-08-06 19:51:28 +0500

    add support view to the pages

---
+++ src/app/pages/pages-support/pages-support.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-support',
+  templateUrl: './pages-support.component.html',
+  styleUrls: ['./pages-support.component.scss']
+})
+export class PagesSupportComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
