d5cb2d97783ed14a6cb9b6d8c795a113a5f31b09
Zain Zafar <zain@aurorasolutions.io>
2018-08-14 01:27:31 +0500

    Fix - Terms of service, Report a bug, upgrade account buttons

--- src/app/mail/mail-footer/mail-footer.component.ts
+++ src/app/mail/mail-footer/mail-footer.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { AppState, UserState } from '../../store/datatypes';
+import { TakeUntilDestroy } from 'ngx-take-until-destroy';
 
+@TakeUntilDestroy()
 @Component({
   selector: 'app-mail-footer',
   templateUrl: './mail-footer.component.html',
   styleUrls: ['./mail-footer.component.scss']
 })
-export class MailFooterComponent implements OnInit {
+export class MailFooterComponent implements OnInit, OnDestroy {
+  readonly destroyed$: Observable<boolean>;
+  public userState: UserState;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.store
+      .select(state => state.user)
+      .takeUntil(this.destroyed$)
+      .subscribe((user: UserState) => {
+        this.userState = user;
+      });
   }
 
+  ngOnDestroy(): void {}
 }
