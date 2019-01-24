e96cbe3673bdbc7243b83a62e478944c1730e4b7
Waseem Abbas <waseemabbas826@yahoo.com>
2019-01-24 22:29:42 +0500

    DEV-498 include `mail-autoresponder` component in `mail-settings`

--- src/app/mail/mail-settings/mail-autoresponder/mail-autoresponder.component.ts
+++ src/app/mail/mail-settings/mail-autoresponder/mail-autoresponder.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Store } from '@ngrx/store';
 import { OnDestroy, TakeUntilDestroy } from 'ngx-take-until-destroy';
 import { Observable } from 'rxjs/Observable';
+import { AppState, Settings, UserState } from '../../../store/datatypes';
 
 @TakeUntilDestroy()
 @Component({
@@ -11,10 +15,21 @@ import { Observable } from 'rxjs/Observable';
 export class MailAutoresponderComponent implements OnInit, OnDestroy {
   readonly destroyed$: Observable<boolean>;
 
-  constructor() {
+  userState: UserState;
+  settings: Settings;
+  errorMessage: string;
+
+  constructor(private store: Store<AppState>,
+              private formBuilder: FormBuilder,
+              private modalService: NgbModal) {
   }
 
   ngOnInit() {
+    this.store.select(state => state.user).takeUntil(this.destroyed$)
+      .subscribe((user: UserState) => {
+        this.userState = user;
+        this.settings = user.settings;
+      });
   }
 
   ngOnDestroy(): void {
