283f6c532762bb83ba7ea4a88dd0c3cdbbbf6aa5
Atif Saddique <atif.saddique4@gmail.com>
2019-04-04 13:47:24 +0500

    DEV-689 fix github broken link issue

--- src/app/shared/pipes/safe.pipe.ts
+++ src/app/shared/pipes/safe.pipe.ts
@@ -107,22 +107,26 @@ export class SafePipe implements PipeTransform {
     }
   }
 
-  replaceLinksInText(inputText) {
-    let replacedText, replacePattern1, replacePattern2, replacePattern3;
+  replaceLinksInText(inputText: string) {
+    if (!(/<[a-z][\s\S]*>/i.test(inputText))) {
+      if (typeof (inputText) === 'string') {
+        // http://, https://, ftp://
+        const urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
 
-    // URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank" rel="noopener">$1</a>');
+        // www. sans http:// or https://
+        const pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
 
-    // URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank" rel="noopener">$2</a>');
+        // Email addresses
+        const emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
 
-    // Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1" rel="noopener">$1</a>');
-
-    return replacedText;
+        inputText = inputText
+          .replace(urlPattern, '<a href="$&">$&</a>')
+          .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+          .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+      }
+      inputText = inputText.replace(/\n/g, '<br>');
+    }
+    return inputText;
   }
 
 }
