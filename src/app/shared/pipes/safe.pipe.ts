f5ac9e866f30cbed9d94cc7c90a8638a0ea1f07f
Atif Saddique <atif.saddique4@gmail.com>
2019-04-04 11:57:03 +0500

    DEV-689 fix github password reset broken link issue

--- src/app/shared/pipes/safe.pipe.ts
+++ src/app/shared/pipes/safe.pipe.ts
@@ -107,27 +107,22 @@ export class SafePipe implements PipeTransform {
     }
   }
 
-  replaceLinksInText(textData) {
-    if (!(/<[a-z][\s\S]*>/i.test(textData))) {
-      let regex = /https?:\/\/[^\s]+/g;
-      if (typeof (textData) === 'string') {
-        let matches = textData.match(regex);
-        if (matches) {
-          matches.forEach((match) => {
-            textData = textData.split(match).join(`<a href="${match}" target="_blank" rel="noopener">${match}</a>`);
-          });
-        }
-        regex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
-        matches = textData.match(regex);
-        if (matches) {
-          matches.forEach((match) => {
-            textData = textData.split(match).join(`<a href="mailto:${match}" target="_blank" rel="noopener">${match}</a>`);
-          });
-        }
-      }
-      textData = textData.replace(/\n/g, '<br>');
-    }
-    return textData;
+  replaceLinksInText(inputText) {
+    let replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    // URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank" rel="noopener">$1</a>');
+
+    // URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank" rel="noopener">$2</a>');
+
+    // Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1" rel="noopener">$1</a>');
+
+    return replacedText;
   }
 
 }
