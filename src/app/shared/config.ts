835e96199cb887d90127f684ef1b50df29ca022b
Atif Saddique <atif.saddique4@gmail.com>
2019-04-15 19:39:29 +0500

    Redirect to onion address for TOR users only for prod

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -81,7 +81,7 @@ export function isTORBrowser() {
     navigator.userAgent.indexOf('Gecko/20100101 Firefox') > -1 &&
     date.getTimezoneOffset() === 0 &&
     (navigator.plugins && navigator.plugins.length === 0)) {
-    if (window.location.hostname === 'ctemplar.com' || window.location.hostname === 'dev.ctemplar.com') {
+    if (window.location.hostname === 'ctemplar.com') {
       window.location.href = `http://ctemplar42u6fulx.onion${window.location.pathname}`;
     }
     return true;
