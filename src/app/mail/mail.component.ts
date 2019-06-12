d19cd86e209810f238f3f0b5216594f3efe1df58
Ahsin Siddique <ahsin.siddique@gmail.com>
2019-06-12 12:50:59 +0500

    display auto-responder banner

--- src/app/mail/mail.component.ts
+++ src/app/mail/mail.component.ts
@@ -4,7 +4,7 @@ import { Store } from '@ngrx/store';
 // Actions
 import { AccountDetailsGet, BlackListGet, GetDomains, GetDomainsSuccess, GetFilters, GetMailboxes, WhiteListGet } from '../store/actions';
 import { TimezoneGet } from '../store/actions/timezone.action';
-import { AppState, UserState } from '../store/datatypes';
+import {AppState, AutoResponder, UserState} from '../store/datatypes';
 import { SharedService } from '../store/services';
 import { ComposeMailService } from '../store/services/compose-mail.service';
 import { GetOrganizationUsers } from '../store/organization.store';
@@ -20,6 +20,8 @@ export class MailComponent implements OnDestroy, OnInit, AfterViewInit {
 
   @ViewChild('composeMailContainer', { static: false, read: ViewContainerRef }) composeMailContainer: ViewContainerRef;
   private isLoadedData: boolean;
+    autoresponder: AutoResponder = {};
+    autoresponder_status = false;
 
   constructor(private store: Store<AppState>,
               private sharedService: SharedService,
@@ -44,6 +46,12 @@ export class MailComponent implements OnDestroy, OnInit, AfterViewInit {
           } else {
             this.store.dispatch(new GetDomainsSuccess([]));
           }
+            if (userState.autoresponder) {
+                this.autoresponder = userState.autoresponder;
+                if (this.autoresponder.autoresponder_active || this. autoresponder.vacationautoresponder_active) {
+                    this.autoresponder_status = true;
+                }
+            }
         }
       });
 
