86187019279b827f59c5428372dd0517e3883b72
Atif Saddique <atif.saddique4@gmail.com>
2020-02-03 16:56:42 +0500

    Fix draft close issue

--- src/app/store/reducers/compose-mail.reducers.ts
+++ src/app/store/reducers/compose-mail.reducers.ts
@@ -118,8 +118,11 @@ export function reducer(state: ComposeMailState = { drafts: {} }, action: Compos
     }
 
     case ComposeMailActionTypes.CLOSE_MAILBOX: {
-      state.drafts[action.payload.id] = { ...state.drafts[action.payload.id], isClosed: true };
-      return { ...state, drafts: { ...state.drafts } };
+      if (action.payload && state.drafts[action.payload.id]) {
+        state.drafts[action.payload.id] = { ...state.drafts[action.payload.id], isClosed: true };
+        return { ...state, drafts: { ...state.drafts } };
+      }
+      return state;
     }
 
     case ComposeMailActionTypes.CLEAR_DRAFT: {
