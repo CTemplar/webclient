ff0a4fce47c359319455eceed2a8d05a766e2194
Atif Saddique <atif.saddique4@gmail.com>
2020-02-07 12:42:44 +0500

    DEV-1404 Add donate pages

---
+++ src/app/pages/pages-donate/payment-options/payment-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-options',
+  templateUrl: './payment-options.component.html',
+  styleUrls: ['./payment-options.component.scss']
+})
+export class PaymentOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
