753fe4a2870b276b883c61f8671b2a755e3abe28
Tech Evanglist <techevanglist@gmail.com>
2020-04-08 19:20:25 +0500

    DEV-1537 Add electron config

---
+++ electron/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+
+let win: BrowserWindow;
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+function createWindow() {
+  win = new BrowserWindow({ width: 1200, height: 800 });
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/../../dist/index.html`),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
