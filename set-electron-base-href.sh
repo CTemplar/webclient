0362861b92bd4edd836f74c78ea712fa0a13169f
the-hidden-hand <thh@ctemplar.com>
2020-04-30 11:00:58 +0200

    Small corrections

--- set-electron-base-href.sh
+++ set-electron-base-href.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+cd dist
 cp index.html electron-index.html
 sed -i -e "s/href=\"\/\"/href=\".\/\"/g" electron-index.html
 rm electron-index.html-e
-
 openssl dgst -sha256 *index.html
