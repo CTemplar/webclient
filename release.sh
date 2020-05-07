bb388e9f5d46c05e418f36c115cd000bf2e5002f
the-hidden-hand <thh@ctemplar.com>
2020-05-07 22:51:19 +0300

    Improve release.sh

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 read -p "Enter new version number: " version
-sed -i '' -e "/version/s/: \".*\"/: \"${version}\"/" package.json
 git fetch -p
 echo "Preparing MASTER branch..."
 git checkout master
 git pull
+sed -i '' -e "/version/s/: \".*\"/: \"${version}\"/" package.json
 git merge dev -m "Release v${version}"
 git push
