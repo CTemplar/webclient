e0404187511e0f9c822b0ed735fe41075023ba2a
the-hidden-hand <thh@ctemplar.com>
2020-05-07 22:59:32 +0300

    Improve release.sh

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ git fetch -p
 echo "Preparing MASTER branch..."
 git checkout master
 git pull
+git merge dev -m "Prepare for v${version}"
 sed -i '' -e "/version/s/: \".*\"/: \"${version}\"/" package.json
-git merge dev -m "Release v${version}"
+git commit -a -m "Release v${version}"
 git push
