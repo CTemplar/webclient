2202de7a10264780ea26dc24119f4929e52b3f00
the-hidden-hand <thh@ctemplar.com>
2020-05-08 01:43:17 +0300

    Improve release.sh

--- release.sh
+++ release.sh
@@ -5,10 +5,29 @@ echo "Preparing DEV branch..."
 git checkout dev
 git pull
 sed -i '' -e "/version/s/: \".*\"/: \"${version}\"/" package.json
-git commit -a -m "Prepare v${version}"
+git commit -am "Prepare for v${version}"
 git push
+git checkout -
+
 echo "Preparing MASTER branch..."
 git checkout master
 git pull
 git merge dev -m "Release v${version}" -s recursive -X theirs
 git push
+
+echo "Preparing GH-PAGES branch..."
+npm run build
+cp dist/index.html dist/404.html # Trick for GH-Pages
+sed -e "s/href=\"\/\"/href=\".\/\"/g" dist/index.html > dist/electron-index.html
+sed -i '' -e "s/#WEBCLIENT_VERSION#/${version}/" dist/README.md
+index_hash=$(openssl dgst -sha256 dist/index.html | awk '{print $NF}')
+e_index_hash=$(openssl dgst -sha256 dist/electron-index.html | awk '{print $NF}')
+sed -i '' -e "s/#INDEX_HASH#/${index_hash}/" dist/README.md
+sed -i '' -e "s/#E_INDEX_HASH#/${e_index_hash}/" dist/README.md
+git checkout -
+git checkout -B gh-pages
+git add -f dist
+git commit -am "Release v${version}"
+git filter-branch -f --prune-empty --subdirectory-filter dist
+git push -f origin gh-pages
+git checkout -
