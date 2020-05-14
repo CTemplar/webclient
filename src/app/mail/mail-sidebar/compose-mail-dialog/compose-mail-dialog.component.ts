2b9d0fd9e918379b0c09e362ccc9fa3533884f6b
The Hidden Hand <45569116+the-hidden-hand@users.noreply.github.com>
2020-05-14 18:37:49 +0000

    Revert "DEV-1601"

--- src/app/mail/mail-sidebar/compose-mail-dialog/compose-mail-dialog.component.ts
+++ src/app/mail/mail-sidebar/compose-mail-dialog/compose-mail-dialog.component.ts
@@ -1,111 +1,107 @@
 import {
-    AfterViewInit,
-    ChangeDetectorRef,
-    Component,
-    ElementRef,
-    EventEmitter,
-    Input,
-    OnDestroy,
-    OnInit,
-    Output,
-    ViewChild
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild
 } from '@angular/core';
-import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
-import {Mail} from '../../../store/models';
-import {ComposeMailComponent} from '../compose-mail/compose-mail.component';
-import {KeyboardShortcutsComponent, ShortcutInput} from 'ng-keyboard-shortcuts';
-import {getComposeMailDialogShortcuts} from '../../../store/services';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Mail } from '../../../store/models';
+import { ComposeMailComponent } from '../compose-mail/compose-mail.component';
+import { KeyboardShortcutsComponent, ShortcutInput } from 'ng-keyboard-shortcuts';
+import { getComposeMailDialogShortcuts } from '../../../store/services';
 
 @Component({
-    selector: 'app-compose-mail-dialog',
-    templateUrl: './compose-mail-dialog.component.html',
-    styleUrls: ['./compose-mail-dialog.component.scss', './../mail-sidebar.component.scss']
+  selector: 'app-compose-mail-dialog',
+  templateUrl: './compose-mail-dialog.component.html',
+  styleUrls: ['./compose-mail-dialog.component.scss', './../mail-sidebar.component.scss']
 })
 export class ComposeMailDialogComponent implements OnInit, AfterViewInit {
-    @Input() public isComposeVisible: boolean;
-    @Input() public receivers: string[];
-    @Input() public draft: Mail;
-    @Input() public isFullScreen: boolean;
+  @Input() public isComposeVisible: boolean;
+  @Input() public receivers: string[];
+  @Input() public draft: Mail;
+  @Input() public isFullScreen: boolean;
 
-    @Output() public hide = new EventEmitter<boolean>();
-    @Output() public minimize = new EventEmitter<boolean>();
-    @Output() public fullScreen = new EventEmitter<boolean>();
+  @Output() public hide = new EventEmitter<boolean>();
+  @Output() public minimize = new EventEmitter<boolean>();
+  @Output() public fullScreen = new EventEmitter<boolean>();
 
-    @ViewChild(ComposeMailComponent) composeMail: ComposeMailComponent;
-    shortcuts: ShortcutInput[] = [];
-    @ViewChild('input') input: ElementRef;
-    @ViewChild(KeyboardShortcutsComponent) private keyboard: KeyboardShortcutsComponent;
+  @ViewChild(ComposeMailComponent) composeMail: ComposeMailComponent;
+  shortcuts: ShortcutInput[] = [];
+  @ViewChild('input') input: ElementRef;
+  @ViewChild(KeyboardShortcutsComponent) private keyboard: KeyboardShortcutsComponent;
 
 
-    isMinimized: boolean;
-    private confirmModalRef: NgbModalRef;
-    mailSubject = '';
-    mailMinimized = '';
+  isMinimized: boolean;
+  private confirmModalRef: NgbModalRef;
+  mailSubject = '';
 
-    constructor(private modalService: NgbModal,
-                private cdr: ChangeDetectorRef) {
+  constructor(private modalService: NgbModal,
+              private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    if (this.draft) {
+      this.mailSubject = this.draft.subject;
     }
+  }
 
-    ngOnInit(): void {
-        if (this.draft) {
-            this.mailSubject = this.draft.subject;
-        }
+  ngAfterViewInit(): void {
+    this.shortcuts = getComposeMailDialogShortcuts(this);
+
+    this.cdr.detectChanges();
+  }
+
+  onClose() {
+    if (this.composeMail.hasData()) {
+      this.saveInDrafts();
+    } else if (this.composeMail.draftMail) {
+      this.discardEmail();
     }
+  }
 
-    ngAfterViewInit(): void {
-        this.shortcuts = getComposeMailDialogShortcuts(this);
+  subjectChanged($event) {
+    this.mailSubject = $event;
+  }
 
-        this.cdr.detectChanges();
+  saveInDrafts() {
+    this.composeMail.saveInDrafts();
+  }
+
+  discardEmail() {
+    this.composeMail.discardEmail();
+  }
+
+  onHide() {
+    this.hideMailComposeDialog();
+  }
+
+  toggleMinimized() {
+    this.isMinimized = !this.isMinimized;
+    this.minimize.emit(this.isMinimized);
+    if (this.isFullScreen) {
+      this.isFullScreen = false;
     }
+  }
 
-    onClose() {
-        if (this.composeMail.hasData()) {
-            this.saveInDrafts();
-        } else if (this.composeMail.draftMail) {
-            this.discardEmail();
-        }
+  toggleFullScreen() {
+    this.isFullScreen = !this.isFullScreen;
+    this.fullScreen.emit(this.isFullScreen);
+    if (this.isMinimized) {
+      this.isMinimized = false;
     }
+  }
 
-    subjectChanged($event) {
-        this.mailSubject = $event;
-        $event.length > 20 ?
-            this.mailMinimized = $event.trim().substr(0, 20) + '...' :
-            this.mailMinimized = $event;
+  private hideMailComposeDialog() {
+    if (this.confirmModalRef) {
+      this.confirmModalRef.dismiss();
     }
-
-    saveInDrafts() {
-        this.composeMail.saveInDrafts();
-    }
-
-    discardEmail() {
-        this.composeMail.discardEmail();
-    }
-
-    onHide() {
-        this.hideMailComposeDialog();
-    }
-
-    toggleMinimized() {
-        this.isMinimized = !this.isMinimized;
-        this.minimize.emit(this.isMinimized);
-        if (this.isFullScreen) {
-            this.isFullScreen = false;
-        }
-    }
-
-    toggleFullScreen() {
-        this.isFullScreen = !this.isFullScreen;
-        this.fullScreen.emit(this.isFullScreen);
-        if (this.isMinimized) {
-            this.isMinimized = false;
-        }
-    }
-
-    private hideMailComposeDialog() {
-        if (this.confirmModalRef) {
-            this.confirmModalRef.dismiss();
-        }
-        this.hide.emit(true);
-    }
+    this.hide.emit(true);
+  }
 
 }
