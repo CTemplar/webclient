9cdec481fb9efd8aa876d022940779480df15e02
The-Hidden-Hand <45569116+The-Hidden-Hand@users.noreply.github.com>
2020-09-15 18:44:14 +0200

    Remove empty space on body content

--- src/app/mail/mail-sidebar/compose-mail/compose-mail.component.ts
+++ src/app/mail/mail-sidebar/compose-mail/compose-mail.component.ts
@@ -1157,7 +1157,7 @@ export class ComposeMailComponent implements OnInit, AfterViewInit, OnChanges, O
         this.isSignatureAdded = true;
         this.mailData.content = this.mailData.content ? this.mailData.content : ' ';
         if (this.selectedMailbox.signature) {
-          this.mailData.content += `\n\n ${this.getPlainText(this.selectedMailbox.signature)}`;
+          this.mailData.content += `\n\n${this.getPlainText(this.selectedMailbox.signature)}`;
         }
         return;
       }
