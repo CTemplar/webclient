e5a46950b104c4e22e6bc05b623e4acb7bf6c608
Kostya Yelskiy <super.dev0714@gmail.com>
2021-01-29 23:07:45 -0400

    Fixed js error on browser-compatibility.js Remove to use IE, Edge

--- src/assets/js/browser-compatibility.js
+++ src/assets/js/browser-compatibility.js
@@ -1,13 +1,28 @@
-function checkBrowserCompatibilty() {
+function checkSupportCryptoBrowser() {
   return (
-    !(!window || 'undefined' == typeof window || !window.crypto || !window.crypto.getRandomValues) ||
-    ('undefined' != typeof window &&
-      'object' == typeof window.msCrypto &&
-      'function' == typeof window.msCrypto.getRandomValues)
+    !(!window || undefined == typeof window || !window.crypto || !window.crypto.getRandomValues) ||
+    (undefined != typeof window && 'object' == typeof window.msCrypto && 'function' == typeof window.msCrypto.getRandomValues)
   );
 }
-!1 === navigator.cookieEnabled && alert('Cookies are required to use CTemplar. Please enable cookies in your browser.'),
-  checkBrowserCompatibilty() ||
-    alert(
-      'A browser that has crypto (A Pseudo Random Number Generator) is required to use CTemplar. Please update your browser to latest version.',
-    );
+
+/**
+ * detect IEEdge
+ */
+function checkSupportBrowser() {
+  var ua = window.navigator.userAgent;
+  // MicroSoft browser
+  if (
+      ua.indexOf('MSIE ') > 0 || 
+      ua.indexOf('Trident/') > 0 || 
+      ua.indexOf('Edge/') > 0
+    )
+  {
+    return false;
+  }
+  // Other browser
+  return true;
+}
+
+!1 === navigator.cookieEnabled && alert('Cookies are required to use CTemplar. Please enable cookies in your browser.'), 
+checkSupportCryptoBrowser() || alert('A browser that has crypto (A Pseudo Random Number Generator) is required to use CTemplar. Please update your browser to latest version.');
+checkSupportBrowser() || alert('We do not support Internet Explorer');
