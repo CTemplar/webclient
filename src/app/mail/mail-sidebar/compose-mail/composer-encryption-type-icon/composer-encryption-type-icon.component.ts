21abe12f49913ef2d0fa485a3a99151997c43c89
Kostya Yelskiy <super.dev0714@gmail.com>
2021-03-26 17:14:29 -0400

    Update autocrypt

--- src/app/mail/mail-sidebar/compose-mail/composer-encryption-type-icon/composer-encryption-type-icon.component.ts
+++ src/app/mail/mail-sidebar/compose-mail/composer-encryption-type-icon/composer-encryption-type-icon.component.ts
@@ -1,23 +1,23 @@
 import { Component, Input, OnInit, SimpleChange } from '@angular/core';
 import { GlobalPublicKey, PGPEncryptionType } from '../../../../store/datatypes';
 
-enum ReceiverEncryptionType {
+enum ComposerEncryptionType {
   RECEIVER_PGP_MIME_INLINE = 'RECEIVER_PGP_MIME_INLINE',
   RECEIVER_END_TO_END = 'RECEIVER_END_TO_END',
   RECEIVER_NONE = 'RECEIVER_NONE',
 }
 
 @Component({
-  selector: 'app-composer-receiver-icon',
-  templateUrl: './composer-receiver-icon.component.html',
-  styleUrls: ['./composer-receiver-icon.component.scss'],
+  selector: 'app-composer-encryption-type-icon',
+  templateUrl: './composer-encryption-type-icon.component.html',
+  styleUrls: ['./composer-encryption-type-icon.component.scss'],
 })
-export class ComposerReceiverIconComponent implements OnInit {
+export class ComposerEncryptionTypeIconComponent implements OnInit {
   @Input() usersKeys: Map<string, GlobalPublicKey>;
 
   @Input() addedItem: any;
 
-  encryptionType: ReceiverEncryptionType;
+  encryptionType: ComposerEncryptionType;
 
   constructor() {}
 
@@ -27,12 +27,12 @@ export class ComposerReceiverIconComponent implements OnInit {
     const keys = this.usersKeys.get(this.addedItem.email);
     if (!keys.isFetching) {
       if (keys.pgpEncryptionType === PGPEncryptionType.PGP_INLINE || keys.pgpEncryptionType === PGPEncryptionType.PGP_MIME) {
-        this.encryptionType = ReceiverEncryptionType.RECEIVER_PGP_MIME_INLINE;
+        this.encryptionType = ComposerEncryptionType.RECEIVER_PGP_MIME_INLINE;
       } else if (keys.key.length > 0) {
         if (keys.key[0].exists === false) {
-          this.encryptionType = ReceiverEncryptionType.RECEIVER_NONE;
+          this.encryptionType = ComposerEncryptionType.RECEIVER_NONE;
         } else {
-          this.encryptionType = ReceiverEncryptionType.RECEIVER_END_TO_END;
+          this.encryptionType = ComposerEncryptionType.RECEIVER_END_TO_END;
         }
       }
     }
