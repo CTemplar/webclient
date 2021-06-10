9d25e7abd49b9fe2460c442daffbbde1e32c2c2c
Kostya Yelskiy <super.dev0714@gmail.com>
2021-06-10 15:15:40 -0300

    Updated as observable for timers

--- src/app/shared/components/countdown-timer/countdown-timer.component.ts
+++ src/app/shared/components/countdown-timer/countdown-timer.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output, ChangeDetectionStrategy
 import { takeWhile } from 'rxjs/operators';
 import { timer } from 'rxjs/internal/observable/timer';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { BehaviorSubject } from "rxjs";
 
 @UntilDestroy()
 @Component({
@@ -15,21 +16,21 @@ export class CountdownTimerComponent implements OnInit {
 
   @Output() finished = new EventEmitter<boolean>();
 
-  days: number;
+  days: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-  hours: number;
+  hours: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-  minutes: number;
+  minutes: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-  seconds: number;
+  seconds: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   ngOnInit() {
     timer(0, 1000)
       .pipe(untilDestroyed(this))
       .pipe(takeWhile(() => this.duration > 0))
-      .subscribe(res => {
+      .subscribe(() => {
         this.calculate();
-        this.duration--;
+        this.duration -= 1;
         if (this.duration === 0) {
           this.finished.emit(true);
         }
@@ -38,12 +39,12 @@ export class CountdownTimerComponent implements OnInit {
 
   calculate() {
     let durationCopy = this.duration;
-    this.days = Math.floor(durationCopy / 86400);
-    durationCopy -= this.days * 86400;
-    this.hours = Math.floor(durationCopy / 3600) % 24;
-    durationCopy -= this.hours * 3600;
-    this.minutes = Math.floor(durationCopy / 60) % 60;
-    durationCopy -= this.minutes * 60;
-    this.seconds = durationCopy % 60;
+    this.days.next(Math.floor(durationCopy / 86400));
+    durationCopy -= this.days.value * 86400;
+    this.hours.next(Math.floor(durationCopy / 3600) % 24);
+    durationCopy -= this.hours.value * 3600;
+    this.minutes.next(Math.floor(durationCopy / 60) % 60);
+    durationCopy -= this.minutes.value * 60;
+    this.seconds.next(durationCopy % 60);
   }
 }
