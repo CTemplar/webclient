b72f8273b50f5a464c4e3a0c881ac28a99423a02
Kostya Yelskiy <super.dev0714@gmail.com>
2021-10-24 21:30:29 -0300

    fixed duplicated id in template, added signin isolated test

--- src/app/users/users-sign-in/users-sign-in.component.spec.ts
+++ src/app/users/users-sign-in/users-sign-in.component.spec.ts
@@ -1,15 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore, MockStore  } from '@ngrx/store/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersSignInComponent } from './users-sign-in.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('UsersSignInComponent', () => {
   let component: UsersSignInComponent;
   let fixture: ComponentFixture<UsersSignInComponent>;
+  
+  let store: MockStore;
 
-  beforeEach(async(() => {
+  beforeEach((() => {
     TestBed.configureTestingModule({
       declarations: [UsersSignInComponent],
+      imports: [FormsModule, ReactiveFormsModule, HttpClientModule, MatSnackBarModule, RouterTestingModule],
+      providers: [provideMockStore({
+        initialState: {
+          auth: {
+            isAuthenticated: false,
+            user: null,
+            errorMessage: null,
+            inProgress: false,
+            signupState: {
+              username: null,
+              password: null,
+              recaptcha: null,
+              currency: 'USD',
+            },
+            resetPasswordErrorMessage: null,
+            isRecoveryCodeSent: false,
+            captcha: {},
+            auth2FA: {},
+            saveDraftOnLogout: false,
+            passwordChangeInProgress: false,
+          },
+        }
+      })]
     }).compileComponents();
+
+    store = TestBed.inject(MockStore);
   }));
 
   beforeEach(() => {
@@ -18,7 +49,23 @@ describe('UsersSignInComponent', () => {
     fixture.detectChanges();
   });
 
+  function updateForm(username: string, password: string, rememberMe: boolean) {
+    component.loginForm.controls['username'].setValue(username);
+    component.loginForm.controls['password'].setValue(password);
+    component.loginForm.controls['rememberMe'].setValue(rememberMe);
+  }
+
+  // Isolated Test
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('component initial state', () => {
+    expect(component.showFormErrors).toBeFalsy();
+    expect(component.isConfirmedPrivacy).toBeUndefined;
+    expect(component.username).toEqual('');
+    expect(component.password).toEqual('password');
+    expect(component.layout).toEqual('alphanumeric');
+    expect(component.isLoading).toBeFalsy();
   });
 });
