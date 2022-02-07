f711cb14bcecca3b5bcb1d4a056f407dd6829964
Prabhu Suriya <prabhusuriya.chennai@gmail.com>
2022-02-07 18:17:38 +0530

    add scrollIntoView function

--- src/app/shared/util/dom-utils.ts
+++ src/app/shared/util/dom-utils.ts
@@ -1,28 +1,6 @@
-export function getScrollParent(node: any): any {
-  const isElement = node instanceof HTMLElement;
-  const overflowY = isElement && window.getComputedStyle(node).overflowY;
-  const isScrollable = overflowY !== 'visible' && overflowY !== 'hidden';
-
-  if (!node) {
-    return null;
-  }
-  if (isScrollable && node.scrollHeight >= node.clientHeight) {
-    return node;
-  }
-
-  return getScrollParent(node.parentNode) || document.body;
-}
-
-export function scrollIntoView(id: any, isFirefox = false, offset = 0): any {
-  // Firefox cuts off top of the body when using scrollIntoView
-  // Ctemplar display problem #437
-  if (isFirefox) {
-    const fragmentDOM: HTMLElement = document.querySelector(`#${id}`);
-    const parent: HTMLElement = getScrollParent(fragmentDOM);
-    if (parent) {
-      parent.scrollTop = fragmentDOM?.offsetTop + offset;
-    }
-  } else {
-    document.querySelector(`#${id}`)?.scrollIntoView({ behavior: 'smooth' });
-  }
+// Firefox cuts off top of the body when using scrollIntoView
+// Ctemplar display problem #437 fix by scroll the top of the page again
+export function scrollIntoView(element: HTMLElement): void {
+  setTimeout(() => element?.scrollIntoView({ behavior: 'smooth' }));
+  setTimeout(() => document.querySelector('app-mail')?.scrollIntoView(), 500);
 }
